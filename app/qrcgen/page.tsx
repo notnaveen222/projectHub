79249279de3454545980eb1e39b4c99b7b19d4d0
Naveen <notnaveen2@gmail.com>
2024-12-29 17:27:54 +0530

    fixed build errors

--- app/qrcgen/page.tsx
+++ app/qrcgen/page.tsx
@@ -1,12 +1,5 @@
 "use client";
-import { ChangeEvent, useState } from "react";
 export default function Qrcgen() {
-  const [url, setUrl] = useState("");
-
-  const handleInput = (e: ChangeEvent<HTMLInputElement>) => {
-    setUrl(e.target.value);
-  };
-
   return (
     <>
       <div className=" rounded-3xl  bg-black  text-3xl h-full w-full flex flex-col justify-start items-start p-5">
@@ -21,7 +14,6 @@ export default function Qrcgen() {
           <input
             className="bg-transparent focus:ring-0 border-[#27272a] border-2 rounded-lg text-base px-2 py-1 h-11 w-[300px] focus:outline-none focus:border-white"
             placeholder="Enter url"
-            onChange={handleInput}
             type="text"
           />
           <button className="text-base border-[#27272a] hover:border-white hover:border-2 transition-all duration-100 ease-in-out border-2 rounded-lg py-1 px-2">
@@ -29,7 +21,6 @@ export default function Qrcgen() {
           </button>
         </div>
         <div>Loading</div>
-        {/* <Image src={qrSrc} alt="Image"></Image> */}
       </div>
     </>
   );
